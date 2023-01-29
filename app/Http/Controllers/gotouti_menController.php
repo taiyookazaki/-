205ab5ee150b1c29f579e575dd1ba9db2a36590f
taiyookazaki <0cdi2222@mail.u-tokai.ac.jp>
2023-01-29 23:22:31 +0900

    データベース△

---
+++ app/Http/Controllers/gotouti_menController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class gotouti_menController extends Controller
+{
+    //
+}
---
+++ database/seeders/PrefectureSeeder.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PrefectureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('prefectures')->insert([
+            'name' => ' 北海道',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '青森',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '岩手',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '宮城',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '秋田',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '山形',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);DB::table('prefectures')->insert([
+            'name' => '福島',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);DB::table('prefectures')->insert([
+            'name' => '茨城',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);DB::table('prefectures')->insert([
+            'name' => '栃木',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);DB::table('prefectures')->insert([
+            'name' => '群馬',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '埼玉',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '千葉',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '東京',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '神奈川',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '新潟',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '富山',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '石川',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '福井',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '山梨',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '長野',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '岐阜',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '静岡',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '愛知',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '三重',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '滋賀',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '京都',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '大阪',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '兵庫',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '奈良',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '和歌山',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '鳥取',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '島根',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '岡山',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '広島',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '山口',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '徳島',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '香川',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '愛媛',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '高知',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+         DB::table('prefectures')->insert([
+            'name' => '福岡',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '佐賀',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '長崎',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '熊本',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '大分',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '宮崎',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '鹿児島',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); DB::table('prefectures')->insert([
+            'name' => '沖縄',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]); 
+    }
+}
